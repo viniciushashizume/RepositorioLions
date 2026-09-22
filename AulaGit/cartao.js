c2704726e393291309396ceafc474cae043f4165
Vinicius Hashizume <viniciushashizume26@gmail.com>
2026-09-22 09:00:48 -0300

    Adicionado o codigo de cartao.js

---
+++ AulaGit/cartao.js
@@ -0,0 +1,9 @@
+let nome = 'Vinicius'
+let idade = 23
+let cidade = 'Ponta Grossa'
+let curso = 'Ciencia da Computação'
+
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Cidade: ${cidade}`)
+console.log(`Curso: ${curso}`)
