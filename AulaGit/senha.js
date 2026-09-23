f2b09c372544e8fb3128ef95d1b16422380582f1
Vinicius Hashizume <viniciushashizume26@gmail.com>
2026-09-23 08:28:01 -0300

    Primeiro commit: adiciona o verificador de senha

---
+++ AulaGit/senha.js
@@ -0,0 +1,9 @@
+let senhaCorreta = "lions2024"
+let senhaDigitada = "lions2024"
+
+if(senhaDigitada === senhaCorreta)
+{
+    console.log("Acesso Liberado")
+}else{
+    console.log("Acesso negado")
+}
