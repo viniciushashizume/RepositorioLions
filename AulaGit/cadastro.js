f90bececb0b794ec1b6af6f34a6e500f6b4389fd
Vinicius Hashizume <viniciushashizume26@gmail.com>
2026-09-23 08:39:53 -0300

    Adiciona Cadastro do aluno

---
+++ AulaGit/cadastro.js
@@ -0,0 +1,10 @@
+let aluno = {
+    nome: "Vinicius",
+    idade: 23,
+    curso: "Javascript",
+    ativo: true
+}
+
+console.log("Nome: " + aluno.nome)
+console.log("Idade: " + aluno.idade)
+console.log("Curso: " + aluno.curso)
