b9976cee748dce8799c3e73665aca890b897fafa
Vinicius Hashizume <viniciushashizume26@gmail.com>
2026-09-23 08:43:53 -0300

    Adiciona verificação de aluno ativo

--- AulaGit/cadastro.js
+++ AulaGit/cadastro.js
@@ -2,9 +2,16 @@ let aluno = {
     nome: "Vinicius",
     idade: 23,
     curso: "Javascript",
-    ativo: true
+    ativo: false
 }
 
 console.log("Nome: " + aluno.nome)
 console.log("Idade: " + aluno.idade)
 console.log("Curso: " + aluno.curso)
+
+if(aluno.ativo === true)
+{
+    console.log("O aluno está ativo")
+}else{
+    console.log("O aluno está inativo")
+}
